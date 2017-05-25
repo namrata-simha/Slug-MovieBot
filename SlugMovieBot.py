afd698709f7dfdb4e80a1f4b469b4b68d0c5a140
Lixue <lixue.zhang@outlook.com>
2017-05-26 07:21:06 +0800

    test with some errors from imdb section. Alif please check the problems.

--- SlugMovieBot.py
+++ SlugMovieBot.py
@@ -2,8 +2,29 @@ import random
 import commands
 import xml.dom.minidom
 from BeautifulSoup import BeautifulSoup
-import re
 from imdb import IMDb
+import re
+
+# question corpus, you can ask this kind of questions.
+# what genre is Forrest Gump?
+# who are the actors in Zootopia?
+qlist = [
+	['what', 'genre', 'is'],
+	['who', 'are', 'the', 'actors', 'in'],
+	['who', 'was', 'the', 'director', 'of'],
+	['who', 'was', 'the', 'writer', 'of'],
+	['what', 'classification', 'does', 'have'],
+	['what', 'year', 'was', 'made', 'in'],
+	['what', 'is', 'the', 'running', 'time', 'of'],
+	['what', 'is', 'the', 'plot', 'of'],
+	['where', 'was', 'made'],
+	['what', 'is', 'a', 'quote', 'from'],
+	['what', 'was', 'the', 'production', 'company', 'behind'],
+	['what', 'is', 'a', 'bit', 'of', 'trivia', 'from'],
+	['what', 'language', 'is', 'in'],
+	['what', 'rating', 'does', 'have'],
+	['who', 'was', 'the', 'producer', 'of']
+]
 
 class SlugMovieBot:
 	def __init__(self):
@@ -19,58 +40,19 @@ class SlugMovieBot:
 			f.close()
 		self.twitter_soup = BeautifulSoup(''.join(self.twitter_data_set))
 		#@ interface among functions
-		self.twitter_index_keywords = "Avatar" #used by TwitterIndex, iMDBIndex should provide this parameter.
-		self.imdb_index_keywords = "" #used by iMDBIndex, QReadAndAnalysis should provide this parameter.
+		self.movie_name = ""
+		self.movie_keywords = ""
+		self.twitter_index_keywords = "" #used by TwitterIndex, iMDBIndex should provide this parameter.
+		#@ twitter classification according to sentiment analysis.
 
-	def QReadAndAnalysis(self):
-		question = input("Q: ")
-		"wrtie the result to imdb_index_keywords"
-		pass
+	def imdbIndex(self, movie_name, idx):
+		return "Tom Hanks"
 
-	#def iMDBIndex(self):
-	#	"wrtie the result to twitter_index_keywords"
-	#	pass
-	
-	# This method takes a string as the movie_name and a list of movie_keywords.
-	# A movie_keyword is some information that you want to know about the movie.
-	# It returns an array with answers to the data requested in movie_keywords.
-	# Valid options in the list of movie_keywords are: director, runtime, actors, 
-	# year released, film rating, imdb rating, writers, languages, plot, plot outline,
-	# producers, production companies, and distributors. If an invalid movie_keyword 
-	# is provided, then None is inserted into the returned array for that value.
-	def imdbIndex(movie_name, movie_keywords):
-		imdbAccess = IMDb()
-		s_result = ia.search_movie(movie_name)
-		if not s_result:
-			return None
-		movie = s_result[0]
-		imdbAccess.update(movie)
-		options = {'director': movie['director'][0], 'runtime': movie['runtime'][0], 'actors': getData(movie, 'cast'), 
-				'year released': movie['year'], 'film rating': movie['mpaa'], 'imdb rating': movie['rating'], 
-				'writers': getData(movie, 'writer'), 'languages': getData(movie, 'languages'), 'plot': getData(movie, 'plot'), 
-				'plot outline': movie['plot outline'], 'producers': getData(movie, 'producer'), 
-				'production companies': getData(movie, 'production companies'), 'distributors': getData(movie, 'distributors')}
-		retrieved_information = []
-		for keyword in movie_keywords:
-			if keyword in options.keys():
-				retrieved_information.append(options[keyword])
-			else:
-				retrieved_information.append(None)
-		return retrieved_information
-	
-	# Returns imdb data as a readable string for person objects, company objects, etc.
-	def getData(movie, option):
-		total_data = ''
-		for data in movie[option]:
-			total_data += str(data) + ', '
-		return total_data
-	
-	# Takes a movie name and option number from 1-15 and outputs the requested data.
-	def imdbData(movie_name, id):
+	def imdbData(self, movie_name, id):
 	    if ((id < 1) or (id > 15)):
 	        return None
 	    imdbAccess = IMDb()
-	    s_result = ia.search_movie(movie_name)
+	    s_result = imdbAccess.search_movie(movie_name)
 	    if not s_result:
 	        return None
 	    movie = s_result[0]
@@ -94,6 +76,33 @@ class SlugMovieBot:
 
 	# retrieve a piece of twitter. 
 	# twitter_index_keywords is the input
+	def tweetSelection(self, very_pos_list, pos_list, very_neg_list, neg_list, neu_list):    
+	    #Deal with small lists
+	    if len(very_pos_list) <= 2:
+	        very_pos_list = pos_list
+	    
+	    if len(very_neg_list) <= 2:
+	        very_neg_list = neg_list    	      
+	    #Search for very positive or negative first
+	    target_list = []
+	    if len(pos_list) >= len(neg_list):
+	        target_list = very_pos_list
+
+	    if len(neg_list) > len(pos_list):
+	        target_list = very_neg_list	       
+	    #Use neutral tweets as last resort       
+	    if len(target_list) <= 2:
+	        target_list = neu_list        
+	    
+	    if len(target_list) <= 2:
+	        print 'Not enough tweets to evaluate'
+	        return	    
+	    sortedtweets = sorted(target_list, key=len)
+	    tweet1 = sortedtweets[-1] #Longest tweet    
+	    tweet2 = sortedtweets[-2]
+	    tweet3 = sortedtweets[-3]        
+	    return tweet1
+
 	def TwitterIndex(self):
 		# step 0: initial information declaration.
 		twitter_query_command = 'IndriRunQuery ' + self.twitter_query_file + \
@@ -102,8 +111,8 @@ class SlugMovieBot:
 										edu.stanford.nlp.sentiment.SentimentPipeline \
 										-file stanford/sentiment.txt'
 		query_parames = "<parameters>\n<index>" + self.twitter_index_path +\
-				"</index>\n<query>\n<number>1</number>\n<text>" + self.twitter_index_keywords +\
-				"\n</text>\n</query>\n<memory>1G</memory>\n<runID>runName</runID>\n \
+				"</index>\n<query>\n<number>1</number>\n<text>#combine( " + self.twitter_index_keywords +\
+				" )\n</text>\n</query>\n<memory>1G</memory>\n<runID>runName</runID>\n \
 				<trecFormat>true</trecFormat>\n</parameters>\n"
 		with open(self.twitter_query_file, 'w') as f:
 			f.write(query_parames)
@@ -127,30 +136,115 @@ class SlugMovieBot:
 			f.close()
 		# step 2: classify twitters into 3 groups: positive, negative, and neutral.
 		# using stanford corenlp.
+		very_pos_list = [] 
+		pos_list = []
+		very_neg_list = []
+		neg_list = [] 
+		neu_list = []
 		(status, query) = commands.getstatusoutput(stanford_corenlp_query_command)
-		sentiment_result = query.split("Adding annotator sentiment\n")[1].split('\n')
+		sentiment_result = query.split("Adding annotator sentiment\n")[1].lower().split('\n')
 		for i in range(1, len(sentiment_result), 2):
-			sentiment = sentiment_result[i].split(" ")[-1].lower()
-			if sentiment=='positive':
-				positive_twitter_list.append(sentiment_result[i-1])
-			elif sentiment=='negative':
-				negative_twitter_list.append(sentiment_result[i-1])
+			sentiment = sentiment_result[i]
+			if sentiment=='  very positive':
+				very_pos_list.append(sentiment_result[i-1])
+			elif sentiment=='  very negative':
+				very_neg_list.append(sentiment_result[i-1])
+			elif sentiment=='  positive':
+				pos_list.append(sentiment_result[i-1])
+			elif sentiment=='  negative':
+				neg_list.append(sentiment_result[i-1])
 			else:
-				neutral_twitter_list.append(sentiment_result[i-1])	
+				neu_list.append(sentiment_result[i-1])	
 		# step 3: return a twitter according some kind of scoring rules.
-		print "pos: ", positive_twitter_list
-		print "neg: ", negative_twitter_list
-		print "neu: ", neutral_twitter_list
-		return twitter
+		"""
+		print "----------------------twitter list to selection module.------------------------"
+		print very_pos_list
+		print pos_list
+		print very_neg_list
+		print neg_list
+		print neu_list
+		print "-------------------------------------------------------------------------------"
+		"""
+		return self.tweetSelection(very_pos_list, pos_list, very_neg_list, neg_list, neu_list)
+		#return "a twitter"
 
-	def chatbot(self):
-		movieAnalysis = QReadAndAnalysis()
-		movieInfo = iMDBIndex()
-		twitterComments = TwitterIndex()
-		print("A: " + twitterComments)
+	# used to detect movie name in Q.	
+	def movie_name_detection(self, question, idx):
+		"""
+		movie name: words between the first and last unmatched word
+		"""
+		movie_name = ""
+		# step 0: remove '.' and '?' in question
+		qcopy = question
+		qlen = len(qcopy) - 1
+		if qcopy[qlen] == '.' or qcopy[qlen] == '?':
+			qcopy = qcopy[0:qlen]
+		qcopy = qcopy.split(" ")
+		# step 1: construct dictionary.
+		qlist_dict = set([])
+		for word in qlist[idx]:
+			qlist_dict.add(word)
+		# step 2: check same.
+		start_idx = -1
+		end_idx = -1
+		for i in range(0, len(qcopy)):
+			if qcopy[i] not in qlist_dict:
+				start_idx = i
+				end_idx = i
+				break
+		for i in range(start_idx+1, len(qcopy)):
+			if qcopy[i] not in qlist_dict:
+				end_idx = i
+
+		if start_idx == -1:
+			return ""
+
+		for i in range(start_idx, end_idx+1):
+			movie_name += (qcopy[i] + ' ')
+		movie_name = movie_name[0:len(movie_name)-1]
+		# return movie name.	
+		return movie_name
+
+	def qsim(self, question_from_user):
+		# step 1: construct input question dictionary.
+		qlen = len(question_from_user) - 1
+		if question_from_user[qlen]=='.' or question_from_user[qlen]=='?':
+			question_from_user = question_from_user[0:qlen]
+		question = question_from_user.lower().split(" ")
+		qdict = set([])
+		for word in question:
+			qdict.add(word)
+		# step 2: select a question from qlist with highest probability.
+		prob = 0
+		idx = 0
+		for i in range(0, len(qlist)):
+			wordcount = 0
+			for word in qlist[i]:
+				if word in qdict:
+					wordcount += 1
+			local_prob = wordcount/float(len(qlist[i]))
+			if local_prob > prob:
+				prob = local_prob
+				idx = i
+		# step 3: return question idx.
+		return idx+1
+
+	def ConversationStrategy(self):
+		# step 0: get user input
+		question_from_user = raw_input("Q: ").lower()
+		# step 1: find similar question from corpus
+		idx = self.qsim(question_from_user)
+		# step 2: retrieve movie name from user input
+		self.movie_name = self.movie_name_detection(question_from_user, idx-1)
+		# step 3: get imdb information.
+		self.twitter_index_keywords = self.imdbData(self.movie_name, idx)
+		self.twitter_index_keywords = self.movie_name + " " + self.twitter_index_keywords
+		# step 4: get twitter.
+		twitter = self.TwitterIndex()
+		# step 5: return 
+		print "A: " + twitter
 
 if __name__=="__main__":
 	moviebot = SlugMovieBot()
-	"""while True:
-		moviebot.chatbot()"""
-	moviebot.TwitterIndex()
+	while True:
+		moviebot.ConversationStrategy()
