6e2de5a63ec58b9764d59b2c3bf843c9c33bede3
Lixue <lixue.zhang@outlook.com>
2017-05-26 08:03:48 +0800

    create a file qlists.py to save questions

--- SlugMovieBot.py
+++ SlugMovieBot.py
@@ -3,28 +3,8 @@ import commands
 import xml.dom.minidom
 from BeautifulSoup import BeautifulSoup
 from imdb import IMDb
+from qlists import qlist
 import re
-
-# question corpus, you can ask this kind of questions.
-# what genre is Forrest Gump?
-# who are the actors in Zootopia?
-qlist = [
-	['what', 'genre', 'is'],
-	['who', 'are', 'the', 'actors', 'in'],
-	['who', 'was', 'the', 'director', 'of'],
-	['who', 'was', 'the', 'writer', 'of'],
-	['what', 'classification', 'does', 'have'],
-	['what', 'year', 'was', 'made', 'in'],
-	['what', 'is', 'the', 'running', 'time', 'of'],
-	['what', 'is', 'the', 'plot', 'of'],
-	['where', 'was', 'made'],
-	['what', 'is', 'a', 'quote', 'from'],
-	['what', 'was', 'the', 'production', 'company', 'behind'],
-	['what', 'is', 'a', 'bit', 'of', 'trivia', 'from'],
-	['what', 'language', 'is', 'in'],
-	['what', 'rating', 'does', 'have'],
-	['who', 'was', 'the', 'producer', 'of']
-]
 
 class SlugMovieBot:
 	def __init__(self):
